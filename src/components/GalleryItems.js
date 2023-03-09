2ae6f04e221efedd611890832f3c4d4ea229e271
Necati Coşkunçopur <necaticoskuncopur@gmail.com>
2023-03-09 18:05:02 +0300

    Update Table

--- src/components/GalleryItems.js
+++ src/components/GalleryItems.js
@@ -1,91 +1,41 @@
-import React, { useState } from "react";
-import { SortableContainer, SortableElement } from "react-sortable-hoc";
-import arrayMove from "array-move";
-
-const GalleryItems = ({ items, setItems, removeItem, editItem, cropModal }) => {
-  const SortableItem = SortableElement(({ item }) => (
-    <div className="gallery-item">
-      <span className="gallery-item--buttons">
-        {item.selectedImage && (
-          <button
-            className=" btn btn-warning text-dark"
-            onMouseDown={() =>
-              cropModal({ open: true, item: item.selectedImage })
-            }
-          >
-            <i className="fas fa-crop"></i>
-          </button>
-        )}
-
-        <button
-          className="edit btn btn-info"
-          onMouseDown={() => editItem(item)}
-        >
-          <i className="fas fa-edit"></i>
-        </button>
-        <button
-          className="btn-close btn btn-danger"
-          onMouseDown={() => removeItem(item.id)}
-        >
-          <i className="fas fa-times"></i>
-        </button>
-      </span>
-      <div className="placeholder">
-        {item?.type === "Images" && (
-          <div className="placeholder-icon">
-            <i className="far fa-image"></i>
-          </div>
-        )}
-        {item?.type === "Videos" && (
-          <div className="placeholder-icon">
-            <i className="fas fa-video"></i>
-          </div>
-        )}
-        {item?.type === "EmbedVideo" && (
-          <div className="placeholder-icon">
-            <i className="fas fa-code"></i>
-          </div>
-        )}
-        {item?.type === "Html" && (
-          <div className="placeholder-icon">
-            <i className="far fa-file-code"></i>
-          </div>
-        )}
-
-        <img
-          src={
-            item?.selectedImage?.thumbnailFullUrl ||
-            item?.selectedVideo?.thumbnailFullUrl ||
-            item?.embedVideo?.thumbnailFullUrl ||
-            item?.html?.thumbnailFullUrl
-          }
-        />
-      </div>
-    </div>
-  ));
-
-  const SortableList = SortableContainer(({ items }) => {
-    const initialItems = window.content_panoramic || {
-      items: [],
-    };
-    console.log(items);
-    return (
-      <div className="gallery-items">
-        {items.map((item, index) => (
-          <SortableItem key={`item-${index}`} index={index} item={item} />
-        ))}
-      </div>
-    );
-  });
-
-  const onSortEnd = ({ oldIndex, newIndex }) => {
-    const sortedList = arrayMove(items, oldIndex, newIndex);
-    setItems(sortedList);
-  };
-
+const GalleryItems = ({ items, removeItem }) => {
+  console.log(items);
   return (
-    <div>
-      <SortableList items={items} onSortEnd={onSortEnd} axis="xy" />
+    <div className="gallery-items">
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">
+              <input type="checkbox" />
+            </th>
+            <th scope="col">Önizleme</th>
+            <th scope="col">Adı</th>
+            <th scope="col">İşlemler</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {items.map((item, index) => (
+            <tr key={`item-${index}`}>
+              <td>
+                <input type="checkbox" />
+              </td>
+              <td>
+                <img
+                  src={item?.thumbnailFullUrl}
+                  alt={item?.thumbnailFullUrl}
+                />
+              </td>
+              <td>{item.name}</td>
+              <td>
+                <button>
+                  <i className="fa fa-trash"></i>
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
